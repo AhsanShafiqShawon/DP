bbc0ba2ed9348dd1190675cb59ed2f49ee9a9701
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2022-10-27 18:12:46 +0600

    changed the solution as I thought

--- Frog 2.cpp
+++ Frog 2.cpp
@@ -1,29 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int dp[100005];
 int arr[100005];
-int k;
+int dp[100005];
+int n, k;
 int func(int pos) {
-	if(pos == 0)
+	if(pos >= n - 1)
 		return 0;
-	int cost = INT_MAX;
+	int val = INT_MAX;
 	if(dp[pos] != -1)	return dp[pos];
 	for(int i = 1; i <= k; i++) {
-		if(pos > i-1) {
-			cost = min(cost, func(pos - i) + abs(arr[pos] - arr[pos - i]));
-		}
+		if(pos + i < n)
+			val = min(val, func(pos + i) + abs(arr[pos] - arr[pos + i]));
 	}
-	return dp[pos] = cost;
+	return dp[pos] = val;
 }
-
 int main() {
-	int n;
 	cin >> n >> k;
 	for(int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
 	memset(dp, -1, sizeof(dp));
-	cout << func(n-1) << '\n';
+	cout << func(0) << '\n';
 	return 0;
 }
